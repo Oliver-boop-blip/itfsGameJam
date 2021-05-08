f508839736f830516ebc35d192081be8d06117db
Oliver-bloop-blip <stolmeye@stud.h-da.de>
2021-05-08 14:39:09 +0200

    Flower Placements

--- ITFSGameJam/Assets/Scripts/CubeBehaviour.cs
+++ ITFSGameJam/Assets/Scripts/CubeBehaviour.cs
@@ -4,36 +4,57 @@ using UnityEngine;
 
 public class CubeBehaviour : MonoBehaviour
 {
+    public GameObject plantPrefab;
+    private void Start()
+    {
+        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject };
+        CubeOverview.Instance.cubeGrid[1, 0].cube.transform.position += new Vector3(0f, 0f, 3f);
+        CubeOverview.Instance.cubeGrid[1, 1].cube.transform.position += new Vector3(0f, 0f, 3f);
+        CubeOverview.Instance.cubeGrid[0, 1].cube.transform.position += new Vector3(0f, 0f, 3f);
+        CubeOverview.Instance.cubeGrid[0, 0].cube.transform.position += new Vector3(0f, 0f, -3f);
+       
+    }
     private void OnMouseDown()
     {
-        for(int i = 0; i < 4; i++)
+        for(int i = 0; i < 8; i++)
         {
-            for(int j = 0; j < 4; j++)
+            for(int j = 0; j < 8; j++)
             {
                 if (j % 2 == 0)
                 {
                     if (transform.position.y == (float)j && transform.position.x == (float)i)
                     {
+                        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject };
+                        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(i + 1, j, 1), Quaternion.identity) as GameObject };
+                        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(i + 0.5f, j + 1, 1), Quaternion.identity) as GameObject };
+                        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(i - 0.5f, j + 1, 1), Quaternion.identity) as GameObject };
+                        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(i - 1, j, 1), Quaternion.identity) as GameObject };
+                        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(i - 0.5f, j - 1, 1), Quaternion.identity) as GameObject };
+                        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(i + 0.5f, j - 1, 1), Quaternion.identity) as GameObject };
                         CubeOverview.Instance.cubeGrid[i, j].cube.transform.position += new Vector3(0f, 0f, -3f);
-                        Debug.Log("Test");
+                      //  Debug.Log("Test");
                     }
                 }
                 if (j % 2 != 0)
                 {
                     if (transform.position.y == (float)j && transform.position.x == (float)i+0.5f)
                     {
+                        new MyPlant { plant = Instantiate(plantPrefab, new Vector3(i+0.5f, j, 0), Quaternion.identity) as GameObject };
                         CubeOverview.Instance.cubeGrid[i, j].cube.transform.position += new Vector3(0f, 0f, -3f);
-                        Debug.Log("Test");
+                     //   Debug.Log("Test");
                     }
                 }
             }
             
         }
     }
-    // Start is called before the first frame update
-    void Start()
+    public class MyPlant
     {
-        
+        public GameObject plant;
+    }
+    public class PlaceableTile
+    {
+        public GameObject placeableTile;
     }
 
     // Update is called once per frame
--- ITFSGameJam/Assets/Scripts/CubeOverview.cs
+++ ITFSGameJam/Assets/Scripts/CubeOverview.cs
@@ -15,7 +15,8 @@ public class CubeOverview : MonoBehaviour
 
     static CubeOverview instance;
     public static CubeOverview Instance
-    {get
+    {
+        get
         { return instance; }
     }
     // Start is called before the first frame update
@@ -26,14 +27,14 @@ public class CubeOverview : MonoBehaviour
     }
     void Start()
     {
-        rows = 4;
-        columns = 4;
+        rows = 8; 
+        columns = 8;
         cubeGrid = new MyCube[rows, columns];
         FillCubeGrid();
     }
     void FillCubeGrid()
     {
-        for(int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < columns; j++)
             {
@@ -51,7 +52,7 @@ public class CubeOverview : MonoBehaviour
         }
         if (j % 2 != 0)
         {
-            cubeGrid[i, j] = new MyCube { cube = Instantiate(cubePrefab, new Vector3(i+0.5f, j, 0), Quaternion.identity) as GameObject };
+            cubeGrid[i, j] = new MyCube { cube = Instantiate(cubePrefab, new Vector3(i + 0.5f, j, 0), Quaternion.identity) as GameObject };
             cubeGrid[i, j].cube.transform.localScale = Vector3.one * hexSize;
         }
     }
@@ -60,17 +61,41 @@ public class CubeOverview : MonoBehaviour
         stonePlace[i, j] = new MyStone { stone = Instantiate(StonePrefab, new Vector3(i, j, 1), Quaternion.identity) as GameObject };
         stonePlace[i, j].stone.transform.localScale = Vector3.one * 0.5f;
     }
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    if (transform.position.y == (float)j && transform.position.x == (float)i)
+                    {
+                        MakeStone(i, j);
+                    }
+                }
+                if (j % 2 != 0)
+                {
+                    if (transform.position.y == (float)j && transform.position.x == (float)i + 0.5f)
+                    {
+                        MakeStone(i, j);
+                    }
+                }
+            }
+
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
-}
-public class MyCube
-{
-    public GameObject cube;
-}
-public class MyStone
-{
-    public GameObject stone;
+    public class MyCube
+    {
+        public GameObject cube;
+    }
+    public class MyStone
+    {
+        public GameObject stone;
+    }
 }
