1fae140d042f00ff55349c9796b1f03613fc867b
Oliver-bloop-blip <stolmeye@stud.h-da.de>
2021-05-08 11:47:22 +0200

    Created the project, I think

---
+++ ITFSGameJam/Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                if (transform.position.y == (float)j && transform.position.x ==(float)i)
+                {
+                    CubeOverview.Instance.cubeGrid[i, j].cube.transform.position += new Vector3(0f, 0f, -3f);
+                }
+            }
+            
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ITFSGameJam/Assets/Scripts/_ubeOverview.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeOverview : MonoBehaviour
+{
+    int rows, columns;
+    public MyCube[,] cubeGrid;
+    public MyStone[,] stonePlace;
+    [SerializeField]
+    GameObject cubePrefab;
+    public float hexSize = 70;
+    [SerializeField]
+    GameObject StonePrefab;
+
+    static CubeOverview instance;
+    public static CubeOverview Instance
+    {get
+        { return instance; }
+    }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance) Destroy(this);
+        else instance = this;
+    }
+    void Start()
+    {
+        rows = 4;
+        columns = 4;
+        cubeGrid = new MyCube[rows, columns];
+        FillCubeGrid();
+    }
+    void FillCubeGrid()
+    {
+        for(int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                MakeCube(i, j);
+            }
+        }
+        MakeStone(1, 2);
+    }
+    void MakeCube(int i, int j)
+    {
+        if (j % 2 == 0)
+        {
+            cubeGrid[i, j] = new MyCube { cube = Instantiate(cubePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject };
+            cubeGrid[i, j].cube.transform.localScale = Vector3.one * hexSize;
+        }
+        if (j % 2 != 0)
+        {
+            cubeGrid[i, j] = new MyCube { cube = Instantiate(cubePrefab, new Vector3(i+0.5f, j, 0), Quaternion.identity) as GameObject };
+            cubeGrid[i, j].cube.transform.localScale = Vector3.one * hexSize;
+        }
+    }
+    void MakeStone(int i, int j)
+    {
+        stonePlace[i, j] = new MyStone { stone = Instantiate(StonePrefab, new Vector3(i, j, 1), Quaternion.identity) as GameObject };
+        stonePlace[i, j].stone.transform.localScale = Vector3.one * 0.5f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public class MyCube
+{
+    public GameObject cube;
+}
+public class MyStone
+{
+    public GameObject stone;
+}
